c9760aedf6ff928ff63ec4b91be6b75c741e46a4
deepakkahar007 <kahardeepak14@gmail.com>
2025-06-29 09:56:37 +0530

    inital commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex flex-1 items-center justify-center bg-black">
+      <Text className="text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
